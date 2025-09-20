db320c32988f19708c5490c2311d6f5319c350c5
Alona Maksymova <alonamaksymova5@gmail.com>
2025-09-20 15:07:38 +0300

    rename app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import CafeInfo from "./CafeInfo/CafeInfo";
-import VoteOptions from "./VoteOptions/VoteOptions";
-import VoteStats from "./VoteStats/VoteStats";
-import Notification from "./Notification/Notification";
-import type { Votes } from "../types/votes";
-import type { VoteType } from "../types/votes";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+import type { Votes } from "../../types/votes";
+import type { VoteType } from "../../types/votes";
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
